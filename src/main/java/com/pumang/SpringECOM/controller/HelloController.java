35fd63758d7d52027e4be9872f6657257393464d
Pumang Kirange <kirangespumang03@gmail.com>
2024-10-21 18:08:08 +0530

    Controllers Addition

---
+++ src/main/java/com/pumang/SpringECOM/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.pumang.SpringECOM.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Welcome To Pumang World";
+    }
+}
